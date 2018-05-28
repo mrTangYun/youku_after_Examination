cb9075235b90409060d418818cbe806c9a97a408
唐云 <tangyun@speedto.cn>
2018-05-28 20:47:34 +0800

    宽度配置

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -212,8 +212,8 @@ module.exports = {
                           }),
 	                        postcssAspectRatioMini({}),
 	                        postcssPxToViewport({
-		                        viewportWidth: 1240, // (Number) The width of the viewport.
-		                        viewportHeight: 2205, // (Number) The height of the viewport.
+		                        viewportWidth: 750, // (Number) The width of the viewport.
+		                        viewportHeight: 1226, // (Number) The height of the viewport.
 		                        unitPrecision: 3, // (Number) The decimal numbers to allow the REM units to grow to.
 		                        viewportUnit: 'vw', // (String) Expected units.
 		                        selectorBlackList: ['.ignore', '.hairlines'], // (Array) The selectors to ignore and leave as px.
