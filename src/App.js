c03797aecfe9dbc90f1337b2c2d5531b1b4d7ba0
唐云 <tangyun@speedto.cn>
2018-05-28 20:40:40 +0800

    基本配置

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Page0 from './page-0';
+class App extends Component {
+	render() {
+		return (
+            <Page0 />
+		);
+	}
+}
+
+export default App;
