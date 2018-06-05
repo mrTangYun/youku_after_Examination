b4910b80257afa152322f712c506d69490857c4d
唐云 <tangyun@speedto.cn>
2018-06-05 11:44:26 +0800

    监听窗口改变大小事件

--- src/App.js
+++ src/App.js
@@ -36,15 +36,23 @@ class App extends Component {
 		window.HollywoodLog && window.HollywoodLog.click('canvasGenerate.click', '画布页.生成', '');
 	};
 
-	componentDidMount() {
-		const LOADING_NODE = document.getElementById('loading');
-		LOADING_NODE && LOADING_NODE.parentNode.removeChild(LOADING_NODE);
-		
+	setViewport = () => {
 		const scaleValue = window.outerWidth / 1080;
 		const metaTag=document.createElement('meta');
 		metaTag.name = "viewport"
 		metaTag.content = `width=1080, initial-scale=${scaleValue}, maximum-scale=${scaleValue}, user-scalable=0`;
 		document.getElementsByTagName('head')[0].appendChild(metaTag);
+	};
+
+	componentDidMount() {
+		const LOADING_NODE = document.getElementById('loading');
+		LOADING_NODE && LOADING_NODE.parentNode.removeChild(LOADING_NODE);
+		// window.onresize = this.setViewport;
+		this.setViewport();
+		window.addEventListener('resize', this.setViewport);
+	}
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.setViewport);
 	}
 	render() {
 		let currentComponent;
